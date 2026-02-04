c75a6d242fb7ee3797f613b7cf4e3561805e3099
Daniel Ojeda Balsera <danojeda205@gmail.com>
2026-02-04 11:25:51 +0100

    creando navbar y las rutas de los componentes dentro de el

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'listado',component:ListadoComponent},
+  {path:'detalle/:date',component:DetalleComponent},//date es para el parametro para buscar las fotos por fecha
+  {path:'contacto',component:ContactoComponent},
+
+  //rutas por defecto
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'**',redirectTo:'/home'}
+
+];
