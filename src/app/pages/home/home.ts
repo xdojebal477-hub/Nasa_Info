46f0b15eb9124792aff86d8e6355968afea2c31f
xdojebal477-hub <xdojebal477@ieshnosmachado.org>
2026-02-05 09:48:34 +0100

    arreglar carrusel, no responde la api tmpco

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NasaService } from '../../services/nasa.service';
+import { Apod } from '../../interfaces/nasa.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
+  private nasaService = inject(NasaService);
+  public listaApod: Apod[] = [];
+
+  ngOnInit(): void {
+    this.nasaService.getApodList(10).subscribe({
+      next: (data) => {
+        this.listaApod = data.slice(0,5);
+      },
+      error: (err) => {
+        console.error('Error fetching APOD list:', err);
+      }
+    });
+  }
 
 }
